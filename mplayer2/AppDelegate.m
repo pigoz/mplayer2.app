0435ee9efc199ba5c0c1cb2bce1b134a3555ea94
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2012-05-06 14:33:55 +0200

    add licensing and copyright information

--- mplayer2/AppDelegate.m
+++ mplayer2/AppDelegate.m
@@ -1,10 +1,19 @@
-//
-//  AppDelegate.m
-//  mplayer2
-//
-//  Created by Stefano Pigozzi on 5/6/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
+/*
+ * This file is part of mplayer2.app.
+ *
+ * mplayer2.app is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * mplayer2.app is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with mplayer2.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #import "AppDelegate.h"
 
